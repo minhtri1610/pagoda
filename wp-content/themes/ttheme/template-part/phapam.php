e97c602eba048d795fdc78a0c338f8ea326a3189
minhtri <epsminhtri@gmail.com>
2019-07-15 23:08:17 +0700

    update master

--- wp-content/themes/ttheme/template-part/phapam.php
+++ wp-content/themes/ttheme/template-part/phapam.php
@@ -12,6 +12,7 @@
                 'posts_per_page' => '5'
             );
             $list_videos = new WP_Query( $args_banner );
+            $cnt_pam = 0;
         ?>
         <?php
             if ( $list_videos->have_posts() ) :
@@ -23,37 +24,45 @@
                 $video_date_post = date('d-m-Y', strtotime(get_the_date()));
                 $video_url = get_the_permalink();
         ?>
-            <div class="video-main">
-                <div class="video-content">
-                    <?php echo do_shortcode('[embedyt] https://www.youtube.com/watch?v=CrGU7KmVXRk[/embedyt]');?>
-                </div>
-                <div class="video-title-content">
-                    <a title="<?= $video_title;?>" href="<?= $video_url;?>"><?= $video_title;?></a>
-                </div>
-                <div class="newest-info video-post-info">
-                    <i class="material-icons tiny">update</i>
-                    <div class="newest-date">  <?= $video_date_post;?></div>
-                    <i class="material-icons tiny">remove_red_eye</i>
-                    <div class="newest-views"><?= $video_hits;?></div>
-                </div>
-            </div>
-            <div class="video-list">
-                <div class="video-thumnail">
-                    <img src="<?= $video_thumnail?>" alt="">
-                </div>
-                <div class="video-title-right">
-                    <a title="<?= $video_title;?>" href="<?= $video_url;?>"><?= $video_title;?></a>
-                    <div class="video-rigth-info">
+            <?php if($cnt_pam == 0){;?>
+                <div class="video-main">
+                    <div class="video-content">
+                        <?php echo do_shortcode('[embedyt]'.$video_link_youtube.'[/embedyt]');?>
+                    </div>
+                    <div class="video-title-content">
+                        <a title="<?= $video_title;?>" href="<?= $video_url;?>"><?= $video_title;?></a>
+                    </div>
+                    <div class="newest-info video-post-info">
                         <i class="material-icons tiny">update</i>
                         <div class="newest-date">  <?= $video_date_post;?></div>
+                        <i class="material-icons tiny">remove_red_eye</i>
+                        <div class="newest-views"><?= $video_hits;?></div>
                     </div>
                 </div>
-            </div>
+                <div class="video-right">
+            <?php } else{ ?>
+                <div class="video-list">
+                    <div class="video-thumnail">
+                        <img src="<?= $video_thumnail?>" alt="">
+                    </div>
+                    <div class="video-title-right">
+                        <a title="<?= $video_title;?>" href="<?= $video_url;?>"><?= $video_title;?></a>
+                        <div class="video-rigth-info">
+                            <i class="material-icons tiny">update</i>
+                            <div class="newest-date">  <?= $video_date_post;?></div>
+                        </div>
+                    </div>
+                </div>
+            <?php }?>
+                
         <?php
+                $cnt_pam++;
                 endwhile;
+                
                 wp_reset_postdata();
             else:
         ?>
+            </div>
             <p class="text-dranger">Không có bài đăng nào!</p>
         <?php        
             endif;
